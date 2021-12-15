69d9e14f997879e5f2441731bfdb9326b5c434a6
sodalite1204 <your.jiny.9@gmail.com>
2021-12-15 22:34:30 +0900

    Add: 공통API 상수데이터 추가

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const BASE_URL_SUULGO = 'http://10.58.1.31:8000';
+
+const API = {
+  login: `${BASE_URL_SUULGO}/users/kakaologin`,
+  main: `${BASE_URL_SUULGO}`,
+  list: `${BASE_URL_SUULGO}/list`,
+  detail: `${BASE_URL_SUULGO}/detail`,
+  survey: `${BASE_URL_SUULGO}/survey`,
+  mypage: `${BASE_URL_SUULGO}/mypage`,
+};
+export default API;
