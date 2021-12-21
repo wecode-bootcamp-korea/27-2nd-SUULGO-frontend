e1f262176191ebd05add7397b3995ddf68cf4f02
eojine94 <eojine294@gmail.com>
2021-12-21 20:23:25 +0900

    ADD : fetch할 때 주소 추가

--- src/config.js
+++ src/config.js
@@ -7,5 +7,6 @@ const API = {
   detail: `${BASE_URL_SUULGO}/detail`,
   survey: `${BASE_URL_SUULGO}/survey`,
   mypage: `${BASE_URL_SUULGO}/mypage`,
+  promise: `${BASE_URL_SUULGO}/users/promise`,
 };
 export default API;
